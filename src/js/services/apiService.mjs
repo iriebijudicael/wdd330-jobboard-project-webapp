63bbcfe8848d25106689c519352323a3f241fffe
iriebijudicael <iriejudicaelelvis225@gmail.com>
2025-10-20 19:06:59 +0000

    I strive to fix fecht API in apiService.mjs

--- src/js/services/apiService.mjs
+++ src/js/services/apiService.mjs
@@ -1,27 +1,14 @@
-const API_BASE_URL =
-  window.location.hostname === 'localhost'
-    ? 'http://localhost:5000/api/jobs'
-    : 'https://your-deployed-api-domain.com/api/jobs';
+// ApiService.mjs
+const API = './data/mock-jobs.json';
 
 export async function fetchJobsFromServer(query = '', filters = {}) {
   const params = new URLSearchParams({
     q: query || '',
     location: filters.location || '',
     type: filters.type || 'all',
-    experience: filters.experience || 'all',
+    experience: filters.experience || 'all'
   });
-
-  const res = await fetch(`${API_BASE_URL}?${params}`);
+  const res = await fetch(`${API}?${params}`);
   if (!res.ok) throw new Error('Failed to fetch jobs from API');
-  return res.json();
-}
-
-export async function postJobToServer(jobData) {
-  const res = await fetch(API_BASE_URL, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(jobData),
-  });
-  if (!res.ok) throw new Error('Failed to post job');
   return res.json();
 }
